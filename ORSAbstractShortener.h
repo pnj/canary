b3cdf00f826471fccb16073ace6cefb50caad750
Nick Toumpelis <nick@toumpelis.me.uk>
2009-04-26 18:50:14 +0300

    Updated file information and added license information in each file. It will make reading the code slightly harder but makes the license more "enforcable". Kudos to Georgios Gousios for the licence tip.

--- ORSAbstractShortener.h
+++ ORSAbstractShortener.h
@@ -1,11 +1,38 @@
+//
+//  ORSAbstractShortener.h
+//  URL Shorteners
+//
+//  Created by Nicholas Toumpelis on 27/12/2008.
+//  Copyright 2008-2009 Ocean Road Software, Nick Toumpelis.
+//
+//  Version 0.7.1
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy 
+//  of this software and associated documentation files (the "Software"), to 
+//  deal in the Software without restriction, including without limitation the 
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+//  sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//  IN THE SOFTWARE.
+
 /*!
  @header ORSAbstractShortener
  @abstract Abstract superclass used for defining URL shorteners. All shorteners
  should be subclasses of this.
  @author Nicholas Toumpelis
  @copyright Nicholas Toumpelis, Ocean Road Software
- @version 0.7
- @updated 2009-04-12
+ @version 0.7.1
+ @updated 2008-12-27
  */
 
 #import <Cocoa/Cocoa.h>
@@ -17,7 +44,7 @@
  @abstract Abstract superclass used for defining URL shorteners. All shorteners
  should be subclasses of this.
  @author Nicholas Toumpelis
- @version 0.6
+ @version 0.7.1
  @updated 2008-12-27
  */
 @interface ORSAbstractShortener : NSObject <ORSShortener> {
--- ORSShortener.h
+++ ORSShortener.h
@@ -1,10 +1,37 @@
+//
+//  ORSShortener.h
+//  URL Shorteners
+//
+//  Created by Nicholas Toumpelis on 18/10/2008.
+//  Copyright 2008-2009 Ocean Road Software, Nick Toumpelis.
+//
+//  Version 0.7.1
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy 
+//  of this software and associated documentation files (the "Software"), to 
+//  deal in the Software without restriction, including without limitation the 
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+//  sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//  IN THE SOFTWARE.
+
 /*!
  @header ORSShortener
  @abstract Protocol to which URL shorteners should comply.
  @author Nicholas Toumpelis
  @copyright Nicholas Toumpelis, Ocean Road Software
- @version 0.7
- @updated 2009-04-12
+ @version 0.7.1
+ @updated 2008-10-18
  */
 
 #import <Cocoa/Cocoa.h>
@@ -14,7 +41,7 @@
  @group URL Shorteners
  @abstract Protocol to which URL shorteners should comply.
  @author Nicholas Toumpelis
- @version 0.6
+ @version 0.7.1
  @updated 2008-10-18
  */
 @protocol ORSShortener
